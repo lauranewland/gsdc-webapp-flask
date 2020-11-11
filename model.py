5cc617075dac2478cf1ff8707aa6bef8bc9ba0b6
Laura Newland <laurasmora101@gmail.com>
2020-11-11 14:53:42 -0600

    Users Class - added additional fields to the database table created Interest Class

--- model.py
+++ model.py
@@ -25,19 +25,19 @@ class Users(db.Model):
     fname = db.Column(db.String(50), nullable=False)
     lname = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    # address = db.Column(db.String(100), nullable=False)
-    # city = db.Column(db.String(50), nullable=False)
-    # zip_code = db.Column(db.String(10), nullable=False)
-    # phone = db.Column(db.String(15), nullable=False)
-    # pref_communication = db.Column(db.String(50), nullable=False)
-    # print_permissions = db.Column(db.Boolean, nullable=False)
-    # password = db.Column(db.String(50), nullable=False)
-    # member_type = db.Column(db.String(100), nullable=False)
-    # member_standing = db.Column(db.String(25), nullable=False)
-    # other_orgs = db.Column(db.Text)
-    # num_of_gsd = db.Column(db.Integer)
-    # num_breedings = db.Column(db.Integer)
-    # # app_date = db.Column(Date)
+    address = db.Column(db.String(100), nullable=True)
+    city = db.Column(db.String(50), nullable=True)
+    zip_code = db.Column(db.String(10), nullable=True)
+    phone = db.Column(db.String(15), nullable=True)
+    pref_communication = db.Column(db.String(50), nullable=True)
+    print_permissions = db.Column(db.Boolean, nullable=True)
+    password = db.Column(db.String(50), nullable=True)
+    member_type = db.Column(db.String(100), nullable=True)
+    member_standing = db.Column(db.String(25), nullable=True)
+    other_orgs = db.Column(db.Text)
+    num_of_gsd = db.Column(db.Integer)
+    num_breedings = db.Column(db.Integer)
+    # app_date = db.Column(Date)
     # # co_app_fname = db.Column(db.String(50))
     # # co_app_lname = db.Column(db.String(50))
     # # co_app_email = db.Column(db.String(100))
@@ -49,5 +49,36 @@ class Users(db.Model):
         return f'<user_id={self.user_id}, fname={self.fname}, lname={self.lname}>'
 
 
+class Interest(db.Model):
+    """Data Model for User Interest"""
+
+    # Creates a table of user interests
+    __tablename__ = 'interests'
+
+    # Defines the Schema for the users interest table
+    interest_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+    obedience = db.Column(db.Boolean)
+    rally = db.Column(db.Boolean)
+    conformation = db.Column(db.Boolean)
+    agility = db.Column(db.Boolean)
+    herding = db.Column(db.Boolean)
+    scentwork = db.Column(db.Boolean)
+    fun_match = db.Column(db.Boolean)
+    shep_o_gram = db.Column(db.Boolean)
+    training = db.Column(db.Boolean)
+    hospitality = db.Column(db.Boolean)
+    fundraising = db.Column(db.Boolean)
+    gsd_fun_day = db.Column(db.Boolean)
+    demo_mn_fair = db.Column(db.Boolean)
+    annual_banquet = db.Column(db.Boolean)
+    breeding = db.Column(db.Boolean)
+    other = db.Column(db.String(100))
+
+    # Add Relationship to Users Table
+    # user = relationship('User', back_populates='interest')
+
+    def __repr__(self):
+        return f'<interest_id={self.interest_id}, obedience={self.obedience}, training={self.training}>'
 
 
