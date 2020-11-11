755f9c8ae3b58488110a59bc87ca983d7bb096b7
Laura Newland <laurasmora101@gmail.com>
2020-11-11 15:10:56 -0600

    Updated create user function - added member_type, removed password

--- crud.py
+++ crud.py
@@ -1,31 +1,29 @@
 from model import Users, Interest, db
 
 
-def create_user(fname, lname, email):
-    """Create and return a new user."""
-    user = Users(fname=fname, lname=lname, email=email)
+# def create_user(fname, lname, email):
+#     """Create and return a new user."""
+#     user = Users(fname=fname, lname=lname, email=email)
+#
+#     # Adds user interest to the database session
+#     db.add(user)
+#
+#     # Commits user interest to the database
+#     db.commit()
+#
+#     return user
 
-    # Adds user interest to the database session
-    db.add(user)
 
-    # Commits user interest to the database
-    db.commit()
+def create_user(fname, lname, email, address, city, zip_code, phone, pref_communication, print_permissions,
+                member_type, other_orgs, num_of_gsd, num_breedings):
+    user = Users(fname=fname, lname=lname, email=email, address=address, city=city, zip_code=zip_code, phone=phone,
+                 pref_communication=pref_communication, print_permissions=print_permissions, member_type=member_type,
+                 other_orgs=other_orgs, num_of_gsd=num_of_gsd, num_breedings=num_breedings)
+
+    db.session.add(user)
+    db.session.commit()
 
     return user
-
-    """This function will be implemented later"""
-# def create_user(fname, lname, email, address, city, zip_code, phone, pref_communication, print_permissions,
-    #   password, member_type, member_standing, other_orgs, num_of_gsd, num_breedings):
-
-    # user = Users(fname=fname, lname=lname, email=email, address=address, city=city, zip_code=zip_code, phone=phone,
-    # pref_communication=pref_communication, print_permissions=print_permissions, password=password,
-    # member_type=member_type, member_standing=member_standing, other_orgs=other_orgs, num_of_gsd=num_of_gsd,
-    # num_breedings=num_breedings)
-
-    # db.session.add(user)
-    # db.session.commit()
-
-    # return user
 
 
 def get_user_by_email(email):
