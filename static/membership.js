884436909adb667559b95dd17e92423ff5f5533c
Laura Newland <laurasmora101@gmail.com>
2020-11-23 19:25:03 -0600

    Added section to toggle table form - commented out

--- static/membership.js
+++ static/membership.js
@@ -6,3 +6,10 @@ const btn = document.querySelector('.btn-toggle');
 btn.addEventListener('click', () => {
     document.body.classList.toggle('dark-theme');
 });
+
+// Display search form when button is clicked
+//$(document).ready(function() {
+//    $('.search-btn').click(function() {
+//        $('.form1').toggle();
+//    });
+//});
