ea2e75678913f18fb94e2c752ee5575a67a488e7
Laura Newland <laurasmora101@gmail.com>
2020-11-27 16:47:31 -0600

    Updated darkmode

--- static/membership.js
+++ static/membership.js
@@ -1,21 +1,21 @@
 'use strict';
 
 //Dark-Mode
-//const btn = document.querySelector('.btn-toggle');
-//
-//btn.addEventListener('click', () => {
-//    document.body.classList.toggle('dark-theme');
-//});
+const btn = document.querySelector(".btn-toggle");
 
+btn.addEventListener("click", function () {
+    alert("hi")
+  document.body.classList.toggle("dark-theme");
+});
 
 // Display search form when button is clicked
-$(document).ready(function(){
-    $("#visibility").css("display", "none");
+//$(document).ready(function(){
+//    $("#visibility").css("display", "none");
 
-    $("#search-btn").click(function() {
-        alert("Yep")
-        $("#visibility").css("display", "block");
-    });
-});
+//    $("#search-btn").click(function() {
+//        alert("Yep")
+//        $("#visibility").css("display", "block");
+//    });
+//});
 
 
