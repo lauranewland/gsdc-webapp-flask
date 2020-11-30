ab66884f29d25e06f07fef5c2cb7eebcb39c9542
Laura Newland <laurasmora101@gmail.com>
2020-11-29 22:44:46 -0600

    Added Javascript function(commented out)

--- static/membership.js
+++ static/membership.js
@@ -18,4 +18,28 @@ btn.addEventListener("click", function () {
 //    });
 //});
 
+// Slide Show Script
+//
+// function displaySlides(){
+//    var i;
+//    var slides = document.getElementsByClassName("slide-ctr");
+//    var dots = document.getElementsByClassName("dot");
+//
+//    for (i = 0; i < slides.length; i++) {
+//        slides[i].style.display = "none";
+//    }
+//
+//    carouselIndex++;
+//
+//    if (carouselIndex > slides.length) {carouselIndex = 1}
+//
+//    for (i = 0; i < dots.length; i++) {
+//        dots[i].className = dots[i].className.replace(" active", "")
+//    }
+//
+//    slides[carouselIndex-1].style.display = "block";
+//    dots[carouselIndex-1].className += " active";
+//    setTimeout(displaySlides, 5000);
+// }
+
 
